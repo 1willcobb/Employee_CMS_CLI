2f9c1fd1af038597877c0b301d05e05666ef1626
1willcobb <cobb.will@gmail.com>
2023-07-25 14:56:44 -0700

    learned about some specific quryies for manager ids

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const express = require('express')
-const { logRandomColor } = require('./helpers/helpers')
+const helpers = require('./helpers/helpers')
 
 chalk.level = 3;
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use('')
-
 
 app.listen(PORT, () => {
-  logRandomColor(`Connected. Server listening on port localhost:${PORT}`)
+  helpers.logRandomColor(`${header}\n\nConnected. Server listening on port localhost:${PORT}`)
 })
 
 
