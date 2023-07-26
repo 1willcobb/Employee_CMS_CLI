beafae6e890ff5bb166442ce537e31fb7816e382
1willcobb <cobb.will@gmail.com>
2023-07-25 23:29:43 -0700

    added something

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const sql = require('mysql2')
+const helpers = require('../helpers/helpers')
+
+const db = sql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    },
+    helpers.logOJ3(`connected to ${process.env.DB}`)
+);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,36 +1,68 @@
+require('dotenv').config()
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const helpers = require('./helpers/helpers')
+const db = require('./config/connection')
+const table = require('table')
+const chalkTable = require('chalk-table')
+
+const options = {
+    leftPad: 2,
+    columns: [
+      { field: "id",     name: chalk.cyan("ID") },
+      { field: "fruit",  name: chalk.magenta("Fruit") },
+      { field: "veggie", name: chalk.green("Vegetable") },
+      { field: "other",  name: chalk.yellow("Other") }
+    ]
+  };
 
 chalk.level = 3;
 
 const Questions = [
   {
-      name: 'Start',
-      message: 'Welcome to the Employee CMS.\nWhat would you like to do',
-      choices: ['View All Departments', 'View All Rolls', 'View All Employees', 'Add a Department', 'Add a Roll', 'Add an Employee', 'Update Employee Role', 'Update Employee Managers', 'View Employees By Manager', 'View Employees By Department', 'Delete Department', 'Delete Roll', 'Delete Employee', 'View Total Utilized Budget of Department']
-      type: 'list'
+      name: 'start',
+      prefix: '=>>>>',
+      suffix: ' <<<<=',
+      message: 'What would you like to do',
+      choices: ['View All Departments', 
+                'View All Rolls', 
+                'View All Employees', 
+                'Add a Department', 
+                'Add a Roll', 
+                'Add an Employee', 
+                'Update Employee Role', 
+                'Update Employee Managers', 
+                'View Employees By Manager', 
+                'View Employees By Department', 
+                'Delete Department', 
+                'Delete Roll', 
+                'Delete Employee', 
+                'View Total Utilized Budget of Department'],
+      type: 'list',
   },
-  {
-      name: 'textColor',
-      message: 'Enter a text color (name or hex format: #00FFAA): ',
-  },
-  {
-      name: 'shapeChoice',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square'],
-      type: 'list'
-  },
-  {
-      name: 'shapeColor',
-      message: 'Enter a shape color (name or hex format: #00FFAA): ',
-      validate: validateColor
-  },
-  {
-      name: 'bg_color',
-      message: 'Enter a background color (name or hex format: #00FFAA): ',
-      validate: validateColor
-  }
 ]
 
 
+const viewAllDepartmentQuestion = () => {
+    db.query('SELECT * FROM employees', function (err,results){
+        console.log('')
+        console.log(chalk.magenta(chalkTable(results)))
+    })
+}
+
+const start = () => {
+    inquirer
+        .prompt(Questions[0])
+        .then((answer) => {
+            if (answer.start === 'View All Departments'){
+                viewAllDepartmentQuestion()
+            }
+    }).then(()=>{start()})
+};
+
+const init = () => {
+    helpers.logRandomColor(helpers.header);
+    start();
+};
+
+init();
