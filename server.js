f7eccdef5d9208c38c9449c66456ccb1f116fc8f
1willcobb <cobb.will@gmail.com>
2023-07-25 20:20:53 -0700

    started inquier

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-const express = require('express')
 const helpers = require('./helpers/helpers')
 
 chalk.level = 3;
-const PORT = process.env.PORT || 3001;
 
-const app = express();
-
-
-app.listen(PORT, () => {
-  helpers.logRandomColor(`${header}\n\nConnected. Server listening on port localhost:${PORT}`)
-})
-
+const Questions = [
+  {
+      name: 'Start',
+      message: 'Welcome to the Employee CMS.\nWhat would you like to do',
+      choices: ['View All Departments', 'View All Rolls', 'View All Employees', 'Add a Department', 'Add a Roll', 'Add an Employee', 'Update Employee Role', 'Update Employee Managers', 'View Employees By Manager', 'View Employees By Department', 'Delete Department', 'Delete Roll', 'Delete Employee', 'View Total Utilized Budget of Department']
+      type: 'list'
+  },
+  {
+      name: 'textColor',
+      message: 'Enter a text color (name or hex format: #00FFAA): ',
+  },
+  {
+      name: 'shapeChoice',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+      type: 'list'
+  },
+  {
+      name: 'shapeColor',
+      message: 'Enter a shape color (name or hex format: #00FFAA): ',
+      validate: validateColor
+  },
+  {
+      name: 'bg_color',
+      message: 'Enter a background color (name or hex format: #00FFAA): ',
+      validate: validateColor
+  }
+]
 
 
